e5ed4b834cf824d570df8566aa91e2c5245c909e
Florian Quirin <info@bytemind.de>
2018-06-15 19:54:08 +0200

    added license and some info during setup

--- sepia-folder/setup.sh
+++ sepia-folder/setup.sh
@@ -2,9 +2,15 @@
 cd sepia-assist-server
 JAR_NAME=$(ls | grep "^sepia-assist.*jar" | tail -n 1)
 echo ""
-echo "Welcome to the SEPIA setup"
-echo "What would you like to do?"
-echo "1: Setup all (except dynamic DNS)"
+echo "Welcome to the SEPIA framework!"
+echo ""
+echo "This little script will help you with the configuration of your server."
+echo "If you are here for the first time please take 5 minutes to read the (MIT) license agreement, especially the part about 'no-warranty' ^_^."
+echo "You can find the license file in your download folder, in the SEPIA app or on one of the SEPIA pages, e.g.:"
+echo "https://github.com/SEPIA-Framework/sepia-assist-server"
+echo ""
+echo "What would you like to do next?"
+echo "1: Setup all components (except dynamic DNS)"
 echo "2: Define new admin and assistant passwords"
 echo "3: Setup dynamic DNS with DuckDNS"
 echo ""
