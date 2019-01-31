70bc3c32cfff024365fd3206930261d2ade1f3c9
Florian Quirin <info@bytemind.de>
2019-01-31 17:21:51 +0100

    included Plugins folder in backup script

--- sepia-custom-bundle-folder/backup-sepia.sh
+++ sepia-custom-bundle-folder/backup-sepia.sh
@@ -10,6 +10,7 @@ mkdir -p $BCK_FOLDER/sepia-teach-server/Xtensions
 mkdir -p $BCK_FOLDER/sepia-websocket-server-java/Xtensions
 mkdir -p $BCK_FOLDER/sepia-reverse-proxy/settings
 cp sepia-assist-server/Xtensions/assist.custom.properties $BCK_FOLDER/sepia-assist-server/Xtensions/
+cp -r sepia-assist-server/Xtensions/Plugins $BCK_FOLDER/sepia-assist-server/Xtensions/
 cp sepia-teach-server/Xtensions/teach.custom.properties $BCK_FOLDER/sepia-teach-server/Xtensions/
 cp sepia-websocket-server-java/Xtensions/websocket.custom.properties $BCK_FOLDER/sepia-websocket-server-java/Xtensions/
 cp sepia-reverse-proxy/settings/proxy.properties $BCK_FOLDER/sepia-reverse-proxy/settings/
