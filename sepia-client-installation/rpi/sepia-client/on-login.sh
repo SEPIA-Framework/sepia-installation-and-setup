a0c4e6b4657f936415fb8595cda8d25981dcb251
Florian Quirin <info@bytemind.de>
2021-10-17 01:18:25 +0200

    fixed a bug in DIY client setup

--- sepia-client-installation/rpi/sepia-client/on-login.sh
+++ sepia-client-installation/rpi/sepia-client/on-login.sh
@@ -3,9 +3,9 @@
 SCRIPT_PATH="$(realpath "$BASH_SOURCE")"
 LOG="$(dirname "$SCRIPT_PATH")""/log-autostart.out"
 NOW=$(date +"%Y_%m_%d_%H:%M:%S")
-echo "Last autostart: $NOW via on-login.sh" > "$LOG"
 # Trigger only on device (ignore SSH logins):
 if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+	echo "Last autostart: $NOW via on-login.sh" > "$LOG"
     client_run_script="$HOME/sepia-client/run.sh"
     if [ "$(cat "$client_run_script" | grep is_headless=1 | wc -l)" = "0" ]; then
 		# Client with display:
