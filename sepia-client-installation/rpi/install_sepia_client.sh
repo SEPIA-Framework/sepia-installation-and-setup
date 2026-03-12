494e2114c693409e05e2df553ea88f05ea9dcf38
Florian Quirin <info@bytemind.de>
2026-03-12 13:29:47 +0100

    removed labwc-environmental and moved

--- sepia-client-installation/rpi/install_sepia_client.sh
+++ sepia-client-installation/rpi/install_sepia_client.sh
@@ -197,9 +197,7 @@ install_wayland() {
 	# Copy labwc config
 	echo "$(log_date) - Copying labwc rc.xml to ~/.config/labwc/rc.xml" >> "$LOG"
 	cp labwc-rc.xml ~/.config/labwc/rc.xml
-	# Copy labwc environment
-	echo "$(log_date) - Copying labwc environment to ~/.config/labwc/environment" >> "$LOG"
-	cp labwc-environment ~/.config/labwc/environment
+
 }
 
 echo "Installing display server: $DISPLAY_MODE ..."
